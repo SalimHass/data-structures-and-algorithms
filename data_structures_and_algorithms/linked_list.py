9c44d2fc2c17cd54b9c23e7d10bd6bc2a8dc2b25
Salim Hassouneh <salim.hassouneh@gmail.com>
2022-03-16 17:08:13 +0300

    kth element

--- data_structures_and_algorithms/linked_list.py
+++ data_structures_and_algorithms/linked_list.py
@@ -29,28 +29,96 @@ class LinkedList:
 
     def __str__(self):
         string = ""
-        if self.head is None:
-            return "The list is empty"
         current = self.head
         while current is not None:
-            string = string + f"{{{current.value}}}->"
+            string = string + f"{{{current.value}}} ->"
             current = current.next_node
         string = string + "NULL"
         return string
 
+    def append(self, value):
+        new_node = Node(value)
+        if self.head:
+            current = self.head
+            while current.next_node:
+                current = current.next_node
+            current.next_node = new_node
+        else:
+            self.head = new_node
 
+    def insert_before(self, value, new_value):
+        if self.head:
+            new_node = Node(new_value)
+            if self.head.value == value:
+                new_node.next_node = self.head
+                self.head = new_node
+                return True
+            else:
+                current = self.head
+                while current.next_node:
+                    if current.next_node.value == value:
+                        new_node.next_node = current.next_node
+                        current.next_node = new_node
+                        return True
+                    current = current.next_node
+        return False
 
+    def insert_after(self, value, new_value):
+        if self.head:
+            new_node = Node(new_value)
+            current = self.head
+            while current.next_node:
+                if current.value == value:
+                    new_node.next_node = current.next_node
+                    current.next_node = new_node
+                    return True
+                current = current.next_node
+        return False
+
+    def delete(self, value):
+        if self.head:
+            if self.head.value == value:
+                self.head = self.head.next_node
+                return True
+            else:
+                current = self.head
+                while current.next_node:
+                    if current.next_node.value == value:
+                        current.next_node = current.next_node.next_node
+                        return True
+                    current = current.next_node
+        return False
+    def kth_from_end(self, k):
+            list_length = 0
+            current = self.head
+            while current is not None:
+                list_length += 1
+                current = current.next_node
+            if 0 <= k < list_length:
+                k_node = self.head
+                target_index = list_length - k
+                index = 0
+                while index < target_index-1:
+                    index += 1
+                    k_node = k_node.next_node
+                return k_node
+            else:
+                raise IndexError
 
 
 if __name__ == '__main__':
-     list = LinkedList()
-     print(list)
-     list.insert(5)
-     list.insert(6)
-     list.insert(7)
-     list.insert(10)
-     print(list)
-     print(list.head.value)
-     print(list.include(10))
-     print(list.include(15))
-     
+    list = LinkedList()
+    print(list)
+    list.insert(5)
+    list.insert(6)
+    list.insert(7)
+    list.insert(10)
+    print(list)
+    print(list.head.value)
+    print(list.include(10))
+    print(list.include(15))
+    list.insert_before(5, 3)
+    list.insert_after(7, 9)
+    list.insert_before(9, 8)
+    print(list)
+    print(list.kth_from_end(3).value)

ead17fc4533fc79b58adf7c94cbe6fb42848d88e
Salim Hassouneh <salim.hassouneh@gmail.com>
2022-03-31 09:04:32 +0300

    Stack and Queue first ch

---
+++ data_structures_and_algorithms/stack_and_queue.md
@@ -0,0 +1,19 @@
+# LinkedList Challenge
+in these challenges we will work on stack and queues :
+### Stack 
+1. push mothod: to add a node to the top of the stack
+2. pop method: to delete top node and returns its value
+3. peek: to return the value of the top node. 
+4. is empty method: returns boolean to check if the stack is empty
+### Queue
+ 5. enqueue:add new node to the back of the queue
+6. dequeue: deletes a value from the front of queue, and returns the value of that node.
+7. peel: returns the value of the front of the queue.
+8. is empty: Returns: Boolean indicating whether or not the queue is empty.
+
+## Whiteboard Process
+![whiteboard](../data_structures_and_algorithms/assessts/stack.jpg)
+![whiteboard](../data_structures_and_algorithms/assessts/Queue.jpg)
+
+## Approach & Efficiency
+we used the stack and queue data structure methods, Node,stack and queue as classes and added functions to handle every requirment
---
+++ tests/test_stack_and_queue.py
@@ -0,0 +1,82 @@
+from data_structures_and_algorithms.stack_and_queue import Stack,Node,Queue
+import pytest
+
+
+
+
+
+def test_push(stack):
+
+
+  actual = stack.top.value
+  expected = 2
+  assert actual == expected
+  
+def test_pop(stack):
+ 
+
+  actual= stack.pop()
+  expected =2
+  assert actual == expected
+
+
+def test_peek_of_stack(stack):
+  
+
+  actual = stack.peek()
+  expected = 2
+  assert actual == expected
+
+
+def test_is_empty_stack():
+  stack=Stack()
+  actual = stack.is_empty()
+  expected = True
+  assert actual == expected
+
+def test_next_item(stack):
+  actual=stack.top.next.value
+  expected = 3
+  assert actual == expected
+  
+def test_enqueue(test_queue):
+  actual=test_queue.front.value
+  expected=13
+  assert actual == expected
+
+def test_dequeue(test_queue2):
+  
+  actual = test_queue2.dequeue()
+  expected=13
+  assert actual == expected
+
+def test_peek(test_queue2):
+  
+  actual = test_queue2.peek()
+  expected=13
+  assert actual == expected
+ 
+@pytest.fixture
+def stack():
+ 
+  stack = Stack()
+  stack.push(5)
+  stack.push(3)
+  stack.push(2)
+
+  return stack
+
+@pytest.fixture
+def test_queue():
+  queue=Queue()
+  queue.enqueue(13)
+  return queue
+
+@pytest.fixture
+def test_queue2():
+  queue=Queue()
+  queue.enqueue(13)
+  queue.enqueue(15)
+  queue.enqueue(17)
+  queue.enqueue(7)
+  return queue

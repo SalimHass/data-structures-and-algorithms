d4abcdccb9cdd3e5ef9621e80be640a8f82d71b2
Salim Hassouneh <salim.hassouneh@gmail.com>
2022-05-21 21:44:33 +0300

    hashtable

---
+++ data_structures_and_algorithms/hashtable.md
@@ -0,0 +1,28 @@
+# hashtables
+we need to implement the Hashtable class, it contains the following fuicntions :
+1. on initiation it should get size as argument , and it will construct a list with lenght of the size and filled with None in every entry in the list .
+2. hash: this function will take a key as an arguemnt, it will convert the key using ascii codes then multipliate it with a prime number then get the mode of it to the size , this number will be the index of the key-value pair in the list.
+
+3. set:takes a key and value as argument, will assign the key-value pair to the index of inside the hashtable list
+
+4. get: this takes key as an argument, and will return the value of it in the hashtable list
+
+5. contains: takes key as an argument ,and will return True if the key is presented in the list , and False if not.
+
+6. keys: this will return a list of all the keys in the list.
+
+
+
+## Whiteboard Process
+![whiteboard](../data_structures_and_algorithms/assessts/hashtable.jpg)
+
+
+
+## Approach & Efficiency
+we created several fucniton to server the purpose of this challenge , each fucntion were created seperatly inside the class as required.
+
+## BigO: 
+time: n
+space:1
+
+## pull request
---
+++ data_structures_and_algorithms/hashtable.py
@@ -0,0 +1,90 @@
+
+
+class Hashtable(object):
+    def __init__(self, size=1024):
+        self.size=size
+        self.map=[None]*size
+    
+    def hash(self, key):
+        ascii_sum= 0
+        for ch in key:
+            ch_ascii=ord(ch)
+            ascii_sum+= ch_ascii
+        hashed_key_indx= (ascii_sum*17)%self.size
+        return hashed_key_indx
+
+  
+
+    def set(self,key,value):
+        indx=self.hash(key)
+        if self.map[indx] is None:
+            self.map[indx]=[[key,value]]
+        else:
+            self.map[indx].append([key,value])
+
+    def get(self,key):
+        indx=self.hash(key)
+        if self.map[indx]==None:
+            raise Exception("key is not in the hashtable")
+        if len(self.map[indx])==1:
+            return self.map[indx][0][1]
+        else:
+            
+            for item in self.map[indx]:
+                
+                if item[0]==key:
+                    
+                    return item[1]
+    
+    def contains(self,key):
+        indx=self.hash(key)
+        if self.map[indx]==None:
+            return False
+        if len(self.map[indx])==1:
+            if self.map[indx][0][0]==key :
+                return True
+            else:
+                return False
+        else:
+            
+            for item in self.map[indx]:
+                
+                if item[0]==key:
+                    
+                    if item[1] :
+                        return True
+            else: 
+                return False
+
+
+    def keys(self):
+        keys=[]
+        
+        for item in self.map:
+            if item:
+                if len(item)==1:
+                    keys.append(item[0][0])
+                else:
+                    for i in item:
+                        keys.append(i[0])
+            
+        return keys
+
+if __name__=="__main__":
+    h= Hashtable()
+    h.set('julia', 'Has')
+    h.set('uljia', 'hass')
+    h.set('salim', 'anwar')
+    print(h.keys())
+    #print(h.map)
+    #print(h.get('julia'))
+    #print(h.get('salim'))
+    #print(h.contains('julai'))
+    #print(h.keys())
+
+        
+
+        
+
+
+
